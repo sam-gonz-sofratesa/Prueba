e88134f40f6eb5e8281c5b6ef2ed42c0979444bf
sam-gonz-sofratesa <sgonzalez@sofratesa.com>
2026-04-15 10:42:25 -0500

    feat(prisma+user): actualizar modelo User y CreateUserUseCase con campos reales

--- backend-microservice/src/user/domain/ports/user.repository.interface.ts
+++ backend-microservice/src/user/domain/ports/user.repository.interface.ts
@@ -1,8 +1,9 @@
 import { UserEntity } from '../entities/user.entity';
 
 export interface IUserRepository {
-  create(user: UserEntity): Promise<UserEntity>;
+  create(entity: UserEntity): Promise<UserEntity>;
   findById(id: string): Promise<UserEntity | null>;
-  findByEmail(email: string): Promise<UserEntity | null>;
+  findByCodigo(codigo_empleado: string): Promise<UserEntity | null>;
+  findByNumeroIdentificacion(numero_identificacion: string): Promise<UserEntity | null>;
   update(id: string, partial: Partial<UserEntity>): Promise<UserEntity>;
 }
