c27303b3896529bca74cc5ead530b59fb7e2a4d7
sam-gonz-sofratesa <sgonzalez@sofratesa.com>
2026-04-15 09:16:24 -0500

    feat(app): fase 9 - registrar UserModule y AuthModule en AppModule

--- backend-microservice/src/app.module.ts
+++ backend-microservice/src/app.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuditLogModule } from './audit_log/audit_log.module';
-import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
     AuditLogModule,
-    ConfigModule.forRoot({ isGlobal: true }),
+    UserModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
